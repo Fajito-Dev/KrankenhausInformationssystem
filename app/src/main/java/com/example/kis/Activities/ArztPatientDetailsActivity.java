cda331f3e5f5e120457f02ce786d7fbae7964c79
Faris <70529556+Fajito@users.noreply.github.com>
2022-12-13 18:16:35 +0100

    LaborPatientDetailsActivity Layout erstellt

--- app/src/main/java/com/example/kis/Activities/ArztPatientDetailsActivity.java
+++ app/src/main/java/com/example/kis/Activities/ArztPatientDetailsActivity.java
@@ -10,21 +10,19 @@ import com.example.kis.Adapters.ArztPatientDetailsAdapter;
 import com.example.kis.R;
 
 public class ArztPatientDetailsActivity extends AppCompatActivity {
-
     RecyclerView recyclerView;
-    String patientArr[];
+    String[] patientArr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_arzt_patienten_details);
 
+        setContentView(R.layout.activity_arzt_patienten_details);
         recyclerView = findViewById(R.id.Recycler);
         patientArr = getResources().getStringArray(R.array.patient_data);
 
         ArztPatientDetailsAdapter Adapter = new ArztPatientDetailsAdapter(this, patientArr);
         recyclerView.setAdapter(Adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
     }
 }
---
+++ app/src/main/java/com/example/kis/Activities/LaborPatientDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.kis.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kis.R;
+
+public class LaborPatientDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_labor_patient_details);
+    }
+}
