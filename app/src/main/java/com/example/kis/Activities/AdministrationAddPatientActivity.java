c6a3b467f83d4876415f4088eb3559454e79d6d9
Faris <70529556+Fajito@users.noreply.github.com>
2022-12-18 20:09:10 +0100

    Aenderung bei Visite (wird beim naechsten Push gefixt) -Admin Add PAtient Activity

---
+++ app/src/main/java/com/example/kis/Activities/AdministrationAddPatientActivity.java
@@ -0,0 +1,16 @@
+package com.example.kis.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kis.R;
+
+public class AdministrationAddPatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_administration_add_patient);
+    }
+}
