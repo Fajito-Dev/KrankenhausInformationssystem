e6ee749ad8dce4b0ef7bbcfb161fe2004e050114
valerian <valerian.schu@stud.h-da.de>
2022-12-20 17:00:34 +0100

    laborCheklistA

--- app/src/main/java/com/example/kis/Activities/LaborChecklistActivity.java
+++ app/src/main/java/com/example/kis/Activities/LaborChecklistActivity.java
@@ -1,10 +1,13 @@
 package com.example.kis.Activities;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.kis.Adapters.LaborRequestsAdapter;
@@ -12,9 +15,8 @@ import com.example.kis.Database.DatabaseHelper;
 import com.example.kis.R;
 
 public class LaborChecklistActivity extends AppCompatActivity {
-
     RecyclerView recyclerView;
-    Button logoutButton;
+    Button btnLogout;
     DatabaseHelper databaseHelper;
 
     @SuppressLint("MissingInflatedId")
@@ -22,12 +24,21 @@ public class LaborChecklistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_labor_checklist);
+        recyclerView = findViewById(R.id.LaborChecklistrecyclerView);
+        btnLogout = findViewById(R.id.LaborChecklistButtonLogout);
 
         databaseHelper = new DatabaseHelper(this);
         LaborRequestsAdapter laborRequestsAdapter = new LaborRequestsAdapter(this,databaseHelper.getEveryPatient(),databaseHelper.getEntryLabor());
+        recyclerView.setAdapter(laborRequestsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        recyclerView = findViewById(R.id.LaborChecklistrecyclerView);
-        logoutButton = findViewById(R.id.LaborChecklistButtonLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
